8e3f278ccfb43445949a737dda55c07038b6ea05
Sachinrautela <rautelasachin26@gmail.com>
2025-08-30 10:55:18 +0530

    updated code after corrections

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,4 +1,4 @@
-const cloudinary = require('cloudinary');
+const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
@@ -11,7 +11,7 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'wandurlust_DEV',
-    allowedformats: ["png","jpg","jpeg"],
+    allowed_formats: ["png","jpg","jpeg","avif"],
   },
 });
 
